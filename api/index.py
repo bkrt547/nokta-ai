43770aca25cf88c02fc861638aa2c59166a3b0c8
Berat <brtky23231623@gmail.com>
2026-06-25 22:48:01 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,8 +2,7 @@ import os
 import sys
 from streamlit.web import cli as stcli
 
-# Vercel'in köşe bucak aradığı o "app" nesnesini burada tanımlıyoruz
+# Vercel'in aradığı o kutsal nesne: app
 def app(environ, start_response):
-    # Streamlit'i arka planda tetikleyen ana mekanizma
     sys.argv = ["streamlit", "run", "yapay.py", "--server.port", "8000", "--server.address", "0.0.0.0"]
     stcli.main()
